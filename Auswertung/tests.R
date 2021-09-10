2bc5c51f67163f0de3d21a480995e01d1ba38479
Sebastian Sauer <ssauer@posteo.de>
2021-09-10 16:21:35 +0200

    update test

--- Auswertung/tests.R
+++ Auswertung/tests.R
@@ -16,8 +16,3 @@ subm_last_names %>%
   setdiff(d_grades$last_name)
 
 
-
-"weissler" %in% d_grades$last_name
-d_grades$last_name[d_grades$first_name == "selin"]
-"selin" %in% d_grades$first_name
-"00158303" %in% d_grades$matrikelnummer
